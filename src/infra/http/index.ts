e3c805c4adb5f92ab8da58c684ef17e74f43b09e
Drummerzzz <jf.interatividade@gmail.com>
2022-04-18 22:38:27 -0300

    :sparkles: feat: add cors config

--- src/infra/http/index.ts
+++ src/infra/http/index.ts
@@ -1,32 +1,38 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { ArticleControllerFactory } from '../../domain/factories';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
+const allowedOrigins = ['http://localhost:8080'];
+
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigins
+};
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors(corsOptions))
 
-
-app.get('/', (req: Request, res: Response) => {
+app.get('/', cors(), (req: Request, res: Response) => {
   res.json({
     url: req.url,
     now: new Date().toISOString()
   })
 });
 
-app.get('/health', (req: Request, res: Response) => {
+app.get('/health', cors(), (req: Request, res: Response) => {
   res.json({
     status: 'ok'
   })
 });
 
 
-app.get('/articles/:url', async (req: Request, res: Response) => {
+app.get('/articles/:url', cors(), async (req: Request, res: Response) => {
   const { url } = req.params
   const factory = new ArticleControllerFactory({
     url
@@ -35,7 +41,7 @@ app.get('/articles/:url', async (req: Request, res: Response) => {
   res.json(article)
 });
 
-app.post('/articles/:url', async (req: Request, res: Response) => {
+app.post('/articles/:url', cors(), async (req: Request, res: Response) => {
   const { url } = req.params
   const { text } = req.body
   const factory = new ArticleControllerFactory({
