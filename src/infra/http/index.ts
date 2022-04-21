f11b6b6d3b3535e87d76286bf27da9dfaed5031a
Drummerzzz <jf.interatividade@gmail.com>
2022-04-21 15:52:25 -0300

    :sparkles: feat: Add domain in white list

--- src/infra/http/index.ts
+++ src/infra/http/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
-const allowedOrigins = ['http://localhost:8080'];
+const allowedOrigins = ['http://localhost:8080', 'https://joaofilho.dev', 'https://editor.joaofilho.dev', 'https://dontpad-frontend.vercel.app/'];
 
 const corsOptions: cors.CorsOptions = {
   origin: allowedOrigins
